e904d298041a4cf0a2112e446ff699c39c844698
Ajab Gul <kakar.ajabgul@gmail.com>
2022-01-26 02:58:38 +0500

    module 4 solution

---
+++ Module-4-solution/script.js
@@ -0,0 +1,9 @@
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < names.length; i++){
+    if (names[i].charAt(0) === "J"){
+        console.log(" GoodBye " + names[i])
+    } else {
+        console.log(" Wellcome " + names[i])
+    }
+}
